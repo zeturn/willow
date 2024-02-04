ffb3dc0469297919e706c21805993065d240b582
zeturn <zhr626@outlook.com>
2024-02-04 10:43:10 +0800

    修改header视图

--- resources/views/components/a.blade.php
+++ resources/views/components/a.blade.php
@@ -1,5 +1,5 @@
 <header x-data="{ slideOverOpen: false }" class="text-gray-600 body-font">
-    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <!-- Branding and Mobile Menu Button -->
         <a class="flex title-font font-medium items-center text-blue-500 mb-4 md:mb-0" href="{{ url('/') }}">
             <span class="ml-3 text-3xl">memeGit</span>
@@ -138,113 +138,112 @@
             
             </div>
         </div>
-        </nav>
-            <!-- Desktop Auth Buttons -->
-            @guest
-                <button class="hidden md:inline-flex items-center border-0 py-1 px-4 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-                    <a href="{{ route('login') }}">Login</a>
-                </button>
-                <button class="hidden md:inline-flex items-center border-0 py-1 px-4 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-                    <a href="{{ route('register') }}">Register</a>
-                </button>
-            @else
-                @if(Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                <div class="ms-3 relative">
-                    <x-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                    <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                                </button>
-                            @else
-                                <span class="inline-flex rounded-md">
-                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
-                                        {{ Auth::user()->name }}
+    </nav>
+        <!-- Desktop Auth Buttons -->
+        @guest
+            <button class="hidden md:inline-flex items-center border-0 py-1 px-4 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+                <a href="{{ route('login') }}">Login</a>
+            </button>
+            <button class="hidden md:inline-flex items-center border-0 py-1 px-4 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+                <a href="{{ route('register') }}">Register</a>
+            </button>
+        @else
+            @if(Laravel\Jetstream\Jetstream::managesProfilePhotos())
+            <div class="ms-3 relative">
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                            <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                            </button>
+                        @else
+                            <span class="inline-flex rounded-md">
+                                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                                    {{ Auth::user()->name }}
 
-                                        <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                    <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                    </svg>
+                                </button>
+                            </span>
+                        @endif
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <!-- Account Management -->
+                        <div class="block px-4 py-2 text-xs text-gray-400">
+                            {{ __('Manage Account') }}
+                        </div>
+
+                        <x-dropdown-link href="{{ route('workstation.index') }}">
+                            {{ __('Workstation') }}
+                        </x-dropdown-link>
+
+                        <x-dropdown-link href="{{ route('profile.show') }}">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+
+                        @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                            <x-dropdown-link href="{{ route('api-tokens.index') }}">
+                                {{ __('API Tokens') }}
+                            </x-dropdown-link>
+                        @endif
+
+                        <div class="border-t border-gray-200 dark:border-gray-600"></div>
+
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}" x-data>
+                            @csrf
+
+                            <x-dropdown-link href="{{ route('logout') }}"
+                                        @click.prevent="$root.submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+                    </x-slot>
+                </x-dropdown>
+            </div>
+            @else
+            <span>{{ Auth::user()->name }}</span>
+            @endif
+        @endguest
+    </div>
+
+    <!-- Slide Over for Mobile -->
+    <div x-show="slideOverOpen" @click.away="slideOverOpen = false" @keydown.window.escape="slideOverOpen = false" class="fixed inset-0 overflow-hidden z-50" style="display: none;">
+        <div class="absolute inset-0 overflow-hidden">
+            <!-- Background overlay, show/hide based on slide-over state. -->
+            <div x-show="slideOverOpen" x-transition:enter="ease-in-out duration-500" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in-out duration-500" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+            
+            <section class="absolute inset-y-0 right-0 pl-10 max-w-full flex" x-transition:enter="transform transition ease-in-out duration-500" x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transform transition ease-in-out duration-500" x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full">
+                <!-- Slide-over panel, show/hide based on slide-over state. -->
+                <div class="relative w-screen max-w-md">
+                    <!-- Panel content -->
+                    <div class="flex h-full flex-col py-6 bg-white shadow-xl overflow-y-scroll">
+                        <div class="px-4 sm:px-6">
+                            <div class="flex items-start justify-between">
+                                <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">Menu</h2>
+                                <div class="ml-3 h-7 flex items-center">
+                                    <button @click="slideOverOpen = false" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-indigo-500">
+                                        <span class="sr-only">Close panel</span>
+                                        <!-- Heroicon name: outline/x -->
+                                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
                                         </svg>
                                     </button>
-                                </span>
-                            @endif
-                        </x-slot>
-
-                        <x-slot name="content">
-                            <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
-                            </div>
-
-                            <x-dropdown-link href="{{ route('workstation.index') }}">
-                                {{ __('Workstation') }}
-                            </x-dropdown-link>
-
-                            <x-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
-                            </x-dropdown-link>
-
-                            @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                                <x-dropdown-link href="{{ route('api-tokens.index') }}">
-                                    {{ __('API Tokens') }}
-                                </x-dropdown-link>
-                            @endif
-
-                            <div class="border-t border-gray-200 dark:border-gray-600"></div>
-
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}" x-data>
-                                @csrf
-
-                                <x-dropdown-link href="{{ route('logout') }}"
-                                            @click.prevent="$root.submit();">
-                                    {{ __('Log Out') }}
-                                </x-dropdown-link>
-                            </form>
-                        </x-slot>
-                    </x-dropdown>
-                </div>
-                @else
-                <span>{{ Auth::user()->name }}</span>
-                @endif
-            @endguest
-        
-
-        <!-- Slide Over for Mobile -->
-        <div x-show="slideOverOpen" @click.away="slideOverOpen = false" @keydown.window.escape="slideOverOpen = false" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out md:hidden" style="display: none;">
-            <div class="absolute inset-0 overflow-hidden">
-                <!-- Background overlay, show/hide based on slide-over state. -->
-                <div x-show="slideOverOpen" x-transition:enter="ease-in-out duration-500" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in-out duration-500" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
-                
-                <section class="absolute inset-y-0 right-0 pl-10 max-w-full flex" x-transition:enter="transform transition ease-in-out duration-500" x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transform transition ease-in-out duration-500" x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full">
-                    <!-- Slide-over panel, show/hide based on slide-over state. -->
-                    <div class="relative w-screen max-w-md">
-                        <!-- Panel content -->
-                        <div class="flex h-full flex-col py-6 bg-white shadow-xl overflow-y-scroll">
-                            <div class="px-4 sm:px-6">
-                                <div class="flex items-start justify-between">
-                                    <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">Menu</h2>
-                                    <div class="ml-3 h-7 flex items-center">
-                                        <button @click="slideOverOpen = false" class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-indigo-500">
-                                            <span class="sr-only">Close panel</span>
-                                            <!-- Heroicon name: outline/x -->
-                                            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                                            </svg>
-                                        </button>
-                                    </div>
                                 </div>
                             </div>
-                            <div class="mt-6 relative flex-1 px-4 sm:px-6">
-                                <!-- Replace with your content -->
-                                <nav class="space-y-1">
-                                    <!-- Your navigation links for mobile, including Login/Register for guests or user menu for authenticated users -->
-                                </nav>
-                                <!-- /End replace -->
-                            </div>
+                        </div>
+                        <div class="mt-6 relative flex-1 px-4 sm:px-6">
+                            <!-- Replace with your content -->
+                            <nav class="space-y-1">
+                                <!-- Your navigation links for mobile, including Login/Register for guests or user menu for authenticated users -->
+                            </nav>
+                            <!-- /End replace -->
                         </div>
                     </div>
-                </section>
-            </div>
+                </div>
+            </section>
         </div>
     </div>
 </header>

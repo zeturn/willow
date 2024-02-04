95490cbe08419b0c2b75606117ed1ecb8b93138e
zeturn <zhr626@outlook.com>
2024-02-04 10:50:07 +0800

    整理laylouts视图

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -4,11 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="description" content="@yield('description', 'description1')">
+        <meta name="description" content="@yield('description', 'memegit page')">
         <meta name="keywords" content="@yield('keywords', 'memegit, Hollowdata')">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-        <title>@yield('title', config('app.name', 'memeGit')) - memeGit</title>
+        <title>@yield('title', config('app.name', 'memeGit')) - {{ config('app.name', 'memeGit') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">

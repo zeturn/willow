62438dbd7c039ad70948db830e5375ea808f3183
zeturn <zhr626@outlook.com>
2024-02-06 15:51:41 +0800

    增加面包屑导航

--- resources/views/entries/versions/show.blade.php
+++ resources/views/entries/versions/show.blade.php
@@ -6,8 +6,21 @@
         <!-- Main Content -->
         <div class="w-full lg:w-3/4 px-4">
             <div class="bg-white rounded-lg p-6 dark:border-gray-700 dark:bg-gray-800 mb-4">
+
+            <nav class="flex justify-between">
+                <ol class="inline-flex items-center mb-3 space-x-3 text-sm text-neutral-500 [&_.active-breadcrumb]:text-neutral-500/80 sm:mb-0">
+                    <li class="flex items-center h-full"><a href="{{ url('/') }}" class="py-1 hover:text-neutral-900"><svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" /><path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" /></svg></a></li>  
+                    <span class="mx-2 text-gray-400">/</span>
+                    <li><a href="{{ route('entry.show.explanation', $version->branch->entry->id) }}" class="inline-flex items-center py-1 font-normal hover:text-neutral-900 focus:outline-none">{{ $version->branch->entry?->name }}</a></li>
+                    <span class="mx-2 text-gray-400">/</span>
+                    <li><a href="{{ route('entry.branch.show.showDemoVersion', $version->branch->id) }}" class="inline-flex items-center py-1 font-normal hover:text-neutral-900 focus:outline-none">{{ $version->branch?->id }}</a></li>
+                    <span class="mx-2 text-gray-400">/</span>
+                    <li><a class="inline-flex items-center py-1 font-normal rounded cursor-default active-breadcrumb focus:outline-none"> {{ $version->name }}</a></li>
+                </ol>
+            </nav>
+
                 <h2 class="text-3xl font-bold mb-4 text-gray-900 dark:text-white">
-                    {{ __('Demo Version') }}: {{ $version->name }}
+                    {{ __('Version') }}: {{ $version->name }}
                 </h2>
 
                 <div class="border-t border-gray-200 pt-6">
@@ -44,11 +57,6 @@
         <!-- Sidebar -->
         <div class="w-full lg:w-1/4 px-4 mt-6 lg:mt-0">
             <div class="bg-white rounded-lg p-6 dark:border-gray-700 dark:bg-gray-800 mb-4">
-                <div class="mb-6">
-                    <a href="{{ route('entry.branch.show.showDemoVersion', $version->branch->id) }}" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-200 ease-in-out">
-                        < {{ __('Back to Branch') }}
-                    </a>
-                </div>
                 <!-- Author and Created At --> 
                 <dl>
                     <div class="mb-4">

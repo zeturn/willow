4e531b230b8497afb540d261e52262d050059faf
zeturn <zhr626@outlook.com>
2024-04-11 21:39:58 +0800

    美化视图

---
+++ resources/views/welcome.blade.copy .php
@@ -0,0 +1,18 @@
+@extends('layouts.page')
+
+@section('title')
+    {{ __('Home') }}
+@endsection
+
+@section('description', 'memeGit是一个人文条目数据库，你可以在这里了解、创建和分享你的文化基因！这里针对迷因的特性针对设计了版本管理工具，帮助任何人轻松的向任何迷因提出自己的理解。')
+@section('keywords', '迷因, meme, HollowData, memeGit')
+
+@section('content')
+    <div class="container mx-auto p-4 dark:bg-gray-900 max-w-7xl">
+        <!-- Full Width Main Column -->
+        <div class="w-full px-4">
+            <h1 class="text-6xl font-extrabold mb-6 text-gray-800 dark:text-white">维护中 （2404常规维护）</h1>
+            <p class="text-xl mb-6 text-gray-800 dark:text-white">预计240411完成</p>
+        </div> <!-- End of Main Column -->
+    </div>
+@endsection

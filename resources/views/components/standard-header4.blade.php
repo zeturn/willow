e4613cb36773d0574e7dd9190c5c6614b362ec98
zeturn <zhr626@outlook.com>
2024-05-17 14:48:56 +0800

    继续推进config name

--- resources/views/components/standard-header4.blade.php
+++ resources/views/components/standard-header4.blade.php
@@ -1,7 +1,7 @@
 <header class="text-gray-600 body-font">
   <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a class="flex title-font font-medium items-center mb-4 md:mb-0"  href="{{ url('/') }}">
-      <span class="ml-3 text-blue-500 font-semibold text-3xl">config('app.name', 'memeGit')</span>
+      <span class="ml-3 text-blue-500 font-semibold text-3xl">{{config('app.name', 'memeGit')}}</span>
     </a>
   </div>
 </header>
